71d73848a58f34d2285fc79873c9bd8f3390521a
Dodo Armando <36740106+dodoarmando@users.noreply.github.com>
2025-06-07 00:18:01 +0700

    Add action column

--- resources/js/components/users/columns.tsx
+++ resources/js/components/users/columns.tsx
@@ -1,6 +1,17 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { type User } from "@/types"
 
+import { FilePen, FileX, MoreHorizontal } from "lucide-react"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const columns: ColumnDef<User>[] = [
     {
         accessorKey: "name",
@@ -10,4 +21,24 @@ export const columns: ColumnDef<User>[] = [
         accessorKey: "email",
         header: "Email",
     },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const user = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="flex items-center">
+                        <MoreHorizontal className="h-4 w-4" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem><FilePen />Edit</DropdownMenuItem>
+                        <DropdownMenuItem><FileX />Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        }
+    }
 ]
